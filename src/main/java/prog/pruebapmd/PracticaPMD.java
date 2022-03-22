e00309cdc26ecd5395c61afbcf0b9e2496b5213b
1daw29 <1daw29@INFORMATICA-129.DOMDPTOINFJMPP.INF>
2022-03-22 16:26:16 +0100

    Done

---
+++ src/main/java/prog/pruebapmd/PracticaPMD.java
@@ -0,0 +1,26 @@
+/*
+* To change this license header, choose License Headers in Project Properties.
+* To change this template file, choose Tools | Templates
+* and open the template in the editor.
+ */
+package prog.pruebapmd;
+
+public class PracticaPMD {
+
+    public static void main(String[] args) {
+
+        int altura = 89;
+        if (altura > 89) {
+            System.out.println("Eres muy alto");
+            System.out.println("Prueba de que GIT funciona.");
+        }
+        PracticaPMD ejemPMD = new PracticaPMD();
+        ejemPMD.metodoDobleReturn();
+    }
+
+    public boolean metodoDobleReturn() {
+        int iValor = 10;
+        return iValor != 5;
+    }
+
+}
